55be14cb526390f51cfad9c4cad32b60ba462a6f
Sagar Kumar <mtech.sagarkumar@gmail.com>
2021-10-13 23:23:33 +0530

    commit

--- HelloWorld/src/CwhLiterals.java
+++ HelloWorld/src/CwhLiterals.java
@@ -4,12 +4,18 @@ public class CwhLiterals {
 	public static void main(String [] args) {
 		
 		byte age=34;
+		int age2=56;
 		char ch='A';
 		float f1=5.6f;
-		double d1=4.66D;
-		
-		System.out.print(age,ch,f1,d1);
-		
+		double d1=4.66;
+		long ageDino=77777777777L;
+		boolean a=true;
+		System.out.println(ageDino);
+		System.out.println(d1);
+		System.out.println(f1);
+		System.out.println(a);
+		String str="Sagar Kumar";
+		System.out.print(str);
 		
 	}
 
