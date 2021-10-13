89f66ecad053e90496786a0362b964bde85d9ee0
Sagar Kumar <mtech.sagarkumar@gmail.com>
2021-10-13 22:58:14 +0530

    java

---
+++ HelloWorld/src/CwhLiterals.java
@@ -0,0 +1,16 @@
+
+public class CwhLiterals {
+	
+	public static void main(String [] args) {
+		
+		byte age=34;
+		char ch='A';
+		float f1=5.6f;
+		double d1=4.66D;
+		
+		System.out.print(age,ch,f1,d1);
+		
+		
+	}
+
+}
