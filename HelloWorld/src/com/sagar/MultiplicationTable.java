64d687ea7bcaf4a5c6ac341d09e04710622a2a7e
Sagar Kumar <mtech.sagarkumar@gmail.com>
2021-10-20 11:37:07 +0530

    added notes

--- HelloWorld/src/com/sagar/MultiplicationTable.java
+++ HelloWorld/src/com/sagar/MultiplicationTable.java
@@ -7,9 +7,10 @@ public class MultiplicationTable {
 		
 		for(int i=1;i<=10;i++) {
 			
-			System.out.printf("%d x %d=%d",n,i,n*i);
-			System.out.println();
+			System.out.printf("%d x %d=%d",n,i,n*i).println();
+			//System.out.println()
 		}
+//		System.out.printf("%d x %d=%d",5,3,15);
 	}
 	
 	
---
+++ HelloWorld/src/com/udemy/KeyBoardShortcuts.java
@@ -0,0 +1,9 @@
+package com.udemy;
+
+public class KeyBoardShortcuts {
+
+	public static void main(String[] args) {
+		System.out.println("sout");
+	}
+
+}
