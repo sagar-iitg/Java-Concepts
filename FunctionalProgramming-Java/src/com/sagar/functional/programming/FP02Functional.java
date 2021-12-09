ff4a0d4479127165d1d6a215853061342c9cd008
Sagar Kumar <mtech.sagarkumar@gmail.com>
2021-12-09 22:54:07 +0530

    collections & generics .txt notes

--- FunctionalProgramming-Java/src/com/sagar/functional/programming/FP02Functional.java
+++ FunctionalProgramming-Java/src/com/sagar/functional/programming/FP02Functional.java
@@ -6,13 +6,15 @@ public class FP02Functional {
 
 	public static void main(String[] args) {
 
-		fun(List.of(12, 9, 10, 5));
+		fun(List.of(12, 9, 10, 5, 10, 23, 34, 56, 78, 0, 2, 34, 33));
 	}
 
 	private static void fun(List<Integer> numbers) {
 
 		// functional Approach
-		numbers.stream().forEach(System.out::println);
+		// numbers.stream().forEach(System.out::println);
+		// Only Even number
+		numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
 
 	}
 
